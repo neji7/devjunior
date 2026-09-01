01f29b9f4754854955eed51bcf0549988de9dcf4
neji7 <nejirayen710@gmail.com>
2026-09-01 17:53:41 +0100

    Add files via upload

--- aap.py
+++ aap.py
@@ -2,30 +2,29 @@ from PIL import Image
 import requests
 import streamlit as st
 from streamlit_lottie import st_lottie
+st.html('<meta name="google-site-verification" content="vc7wFcPIpo7f47jWRdBHdczk538ArRmWJDPd3E9aqjE" />')
+
+st.set_page_config(page_title="My webpage", page_icon=":tada:", layout="wide")
 
 
-
-
-st.set_page_config(page_title="My webpage",page_icon=":tada:",layout="wide")
-
-
-    
 def load_lottieurl(url):
     r = requests.get(url)
     if r.status_code != 200:
         return None
     return r.json()
 
-    
+
 lottie_coding = load_lottieurl("https://lottie.host/8e9ce5db-cfd7-40dd-84db-3ba776b29f5c/SyCbfqoUiu.json")
 
-image_contact_form=Image.open("code.png")
+image_contact_form = Image.open("code.png")
+
+
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("style.css")
 
+local_css("style.css")
 
 with st.container():
     st.subheader("Hi I am rayen :wave: ")
@@ -34,9 +33,6 @@ with st.container():
     st.write("[More info>] (https://www.instagram.com/rayen___neji?igsi=MTJxOW4wNDIwMjdjcg%3D%3D)")
     st.write("[drop a folow to mkni>] (https://www.instagram.com/rayen_makni_/?utm_source=ig_web_button_share_sheet)")
 
-
-
-        
 with st.container():
     st.write("---")
     left_column, right_column = st.columns(2)
@@ -57,33 +53,30 @@ with st.container():
             """
         )
     with right_column:
-            st_lottie(lottie_coding, height=300, key="coding")
-
+        st_lottie(lottie_coding, height=300, key="coding")
 
 with st.container():
-        st.write("---")
-        image_column,text_column=st.columns((1,2))
+    st.write("---")
+    image_column, text_column = st.columns((1, 2))
 
-        with image_column:
+    with image_column:
+        st.image(image_contact_form)
 
-            st.image(image_contact_form)
+    with text_column:
+        st.subheader("Lean how to code:")
+        st.write(
 
-        with text_column:
-            st.subheader("Lean how to code:")
-            st.write(
-
-                """
-                learn how to create a web site with python and streamlit
-                """
-               )
-            st.markdown("[Watch tuto>] (https://youtu.be/VqgUkExPvLY?si=aRfVM_Y9qJ5Tza7-)")
-
+            """
+            learn how to create a web site with python and streamlit
+            """
+        )
+        st.markdown("[Watch tuto>] (https://youtu.be/VqgUkExPvLY?si=aRfVM_Y9qJ5Tza7-)")
 
 with st.container():
     st.write("---")
     st.header("Contact Me :email:")
     st.write("##")
-    contact_form="""
+    contact_form = """
     <form action="https://formsubmit.co/nejirayen710@gmail.com" method="POST" target="_blank">
        <input type="hidden" name="_captcha" value="false">
        <input type="text" name="name" placeholder="your name"  required>
@@ -92,9 +85,9 @@ with st.container():
        <button type="submit">Send</button>
     </form>
     """
-    left_column,right_column=st.columns(2)
+    left_column, right_column = st.columns(2)
     with left_column:
-        st.markdown(contact_form,unsafe_allow_html=True)
+        st.markdown(contact_form, unsafe_allow_html=True)
     with right_column:
         st.empty()
 
@@ -102,4 +95,3 @@ with st.container():
 
 
 
-        
